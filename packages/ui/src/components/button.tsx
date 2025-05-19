56d0ea6e22e9b8e7681e8957dd88552a9b6f73a3
estebanarriaga <estebanarriaga200@gmail.com>
2025-05-19 16:34:33 +0200

    chore: update package.json for globals.css path and add @tailwindcss/cli dependency; enhance button variants with xl size

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -26,6 +26,7 @@ const buttonVariants = cva(
         xs: "h-7 rounded-md gap-1.5 px-3.5 has-[>svg]:px-3.5 text-xs",
         sm: "h-8 rounded-md gap-1.5 px-3.5 has-[>svg]:px-2.5 text-sm",
         lg: "h-12 rounded-md px-3.5 w-[258px] has-[>svg]:px-4 text-sm",
+        xl: "h-14 px-16",
         icon: "size-6.5",
       },
     },
